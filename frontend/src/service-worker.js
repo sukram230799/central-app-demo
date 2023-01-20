ee1ea857d3405a90780b40211585aaf4927b2a49
Markus Wuest <markus.wuest@hpe.com>
2023-01-20 13:10:11 +0100

    Add `SKIP_WAITING` to service-worker

--- frontend/src/service-worker.js
+++ frontend/src/service-worker.js
@@ -4,6 +4,12 @@ cleanupOutdatedCaches();
 
 precacheAndRoute(self.__WB_MANIFEST);
 
+self.addEventListener('message', (event) => {
+    if (event.data && event.data.type === 'SKIP_WAITING')
+        self.skipWaiting();
+});
+
+
 self.addEventListener('push', event => {
     let data = event.data.json();
     const image = 'https://central.wuest.dev/icons/512x512.png';
